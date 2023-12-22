79c2a56a3dd1a032ca0ff02c2a1e15186af1b132
thejzain <thejzainkp@gmail.com>
2023-12-22 22:29:15 +0000

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./routes/home.jsx"
+import Footer from "./components/footer.jsx"
+
 export default function App() {
   return(
-    <div>
-      hello
+    <div className="bg-black p-5 text-white">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/routes/home.jsx
@@ -0,0 +1,12 @@
+import Carousel from "../components/carousel.jsx"
+
+export default function Home() {
+  return (
+      <div classname="bg-black ">
+        hi
+      <div>
+        <Carousel/>
+      </div>
+      </div>
+  );
+}
