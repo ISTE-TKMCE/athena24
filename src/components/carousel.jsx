50cac7e19cddab08620ee8a526e8e9c8e24d0868
Nandagopal vs <nandagopalvs12@gmail.com>
2023-12-26 20:47:15 +0530

    added carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,7 +1,38 @@
+import useEmblaCarousel from "embla-carousel-react";
+
+const slides = [
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+  { url: "https://picsum.photos/200" },
+ 
+];
+
+
 export default function Carousel() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
  return(
- <div>
-      Carousel
+  <div className='embla embla__viewport' ref={emblaRef}>
+    <div className='flex flex-row '>
+    {slides.map((slides,index) => (
+            <div className="embla__slide " key={index}>
+              <div className="">
+                <span>{index + 1}</span>
+              </div>
+              <img
+                className="embla__slide__img"
+                src={slides.url}
+                alt="Your alt text"
+              />
+            </div>
+          ))}
+    </div>
   </div>
- ) 
+);
 }
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div classname="bg-black ">
        <Hero/> 
       <div>
-        <Carousel/>
+       <Carousel/>
       </div>
       </div>
   );
