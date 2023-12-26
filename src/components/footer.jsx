2fabe7eab95733046f136b68fe80133378e1d207
Kiran-347 <kiran333krishna@gmail.com>
2023-12-26 18:58:06 +0530

    first commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,19 @@
 export default function Footer() {
   return(
-  <div>
-      Footer
-  </div>
+  <>
+    <footer className="h-full w-full bg-yellow-400 ">
+      <div className="contianer p-2 h-full w-full grid grid-cols-4">
+        <div className="img-container p-2 ">
+          <img className=" w-44 h-ful " src="src\assets\ISTE original logo black 1 (2).png" alt="" />
+        </div>
+        <div className="main-container text-black col-span-2 text-center">
+          <h2 className="text-3xl font-bold">Contact Us</h2>
+        </div>
+        <div className="img-container p-2 ">
+          <img className=" w-48 h-full float-right " src="src\assets\ISTE original logo black 1(2).png" alt="" />
+        </div>
+      </div>
+    </footer>
+  </>
   )
-  
 }
