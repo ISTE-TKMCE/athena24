23613e0a3bbe3e90199a61aac8e68911fa08665b
Akash Prasad <akashprasadmk@gmail.com>
2023-12-29 20:23:54 +0530

    removed all blur in hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
-/*used custom color.*/
 const Hero = () => (
-  <section className="min-h-screen relative bg-gradient-to-r from-hero-bg via-my-black to-hero-bg text-white">
-     <div className="h-80 absolute inset-y-0 left-0 right-0 bottom-0 bg-gradient-to-b from-my-black to-transperant"></div>
-    <img src="src\assets\iste_owl.png" alt="Image description" className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full object-contain" />
+  <section className="min-h-screen relative bg-hero-bg text-white">
+    <div className="absolute inset-y-0 left-0 right-0 bottom-0 bg-my-black"></div>
+    <img
+      src="src\assets\iste_owl.png"
+      alt="Image description"
+      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full object-contain"
+    />
     <div className="absolute inset-0 flex items-center justify-center text-center">
       <div>
         <h1 className="text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl font-bold mt-80 lg:mt-80 text-my-white">
-        <span
-              className="bg-gradient-to-r from-hero-bg via-my-black to-hero-bg text-transparent bg-clip-text"
-              style={{ backgroundClip: 'text' }}
-            >
-              ATHENA
-            </span>
+          ATHENA
         </h1>
         <p className="text-3xl sm:text-3xl lg:text-5xl font-bold text-my-white">
           24
