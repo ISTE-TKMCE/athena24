02a5af4ed1d6b9f22d440d6a28f6657ba366e29b
thejzain <thejzainkp@gmail.com>
2023-12-29 19:38:13 +0530

    blur component

---
+++ src/components/blur.jsx
@@ -0,0 +1,9 @@
+export default function Blur() {
+  return(
+  <div>
+      blur
+    </div>
+    
+  )
+    
+}
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,12 @@
 import Carousel from "../components/carousel.jsx"
 import Hero from "../components/hero.jsx"
+import Blur from "../components/blur.jsx"
 
 export default function Home() {
   return (
       <div >
        <Hero/> 
+       <Blur/>
       <div>
        <Carousel/>
       </div>
