8d07468f5830e92e7933b901cae0b7bf3ad8104c
Akash Prasad <akashprasadmk@gmail.com>
2023-12-30 00:45:46 +0530

    backblur component added(partial)

--- src/components/blur.jsx
+++ src/components/blur.jsx
@@ -1,9 +1,29 @@
-export default function Blur() {
-  return(
-  <div>
-      blur
-    </div>
-    
-  )
-    
-}
+// Blur.jsx
+import React from "react";
+import PropTypes from "prop-types";
+
+const Blur = ({ position, backgroundColor, children }) => (
+  <div
+    className={`relative ${position} ${backgroundColor} overflow-hidden backdrop`}
+    style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: `radial-gradient(circle, #43021C, transparent)`,
+      color: "white",
+    }}
+  >
+    {children}
+    <div className="absolute inset-y-0 left-0 right-0 bottom-0"></div>
+  </div>
+);
+
+Blur.propTypes = {
+  position: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Blur;
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,23 +1,25 @@
+// Hero.jsx
 import React from "react";
+import Blur from "./blur";
 
 const Hero = () => (
-  <section className="min-h-screen relative bg-hero-bg text-white">
-    <div className="absolute inset-y-0 left-0 right-0 bottom-0 bg-my-black"></div>
+  <section className="min-h-screen relative overflow-hidden text-white">
+      <Blur></Blur>
     <img
       src="src\assets\iste_owl.png"
       alt="Image description"
       className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full object-contain"
     />
     <div className="absolute inset-0 flex items-center justify-center text-center">
-      <div>
-        <h1 className="text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl font-bold mt-80 lg:mt-80 text-my-white">
-          ATHENA
-        </h1>
-        <p className="text-3xl sm:text-3xl lg:text-5xl font-bold text-my-white">
-          24
-        </p>
+        <div>
+          <h1 className="text-6xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl font-bold text-my-white mt-72">
+            ATHENA
+          </h1>
+          <p className="text-3xl sm:text-3xl lg:text-5xl font-bold text-my-white">
+            24
+          </p>
+        </div>
       </div>
-    </div>
   </section>
 );
 
