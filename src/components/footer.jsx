0b0e720be833a55e9f4b6b8562d0141b0adab5ba
Kiran-347 <kiran333krishna@gmail.com>
2023-12-29 12:17:28 +0530

    media icons

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,20 @@ export default function Footer() {
           <img className=" w-44 h-ful " src="src\assets\ISTE original logo black 1 (2).png" alt="" />
         </div>
         <div className="main-container text-black col-span-2 text-center">
-          <h2 className="text-3xl font-bold">Contact Us</h2>
+          <h2 className="text-3xl font-bold">Contact Us</h2><br />
+          <div className="flex justify-between">
+            <a href="#"><img className="w-10 h-10" src="src\assets\ig.png"></img></a>
+            <a href="#"><img className="w-10 h-10" src="src\assets\x new(1).jpg"></img></a>
+            <a href="#"><img className="w-10 h-10" src="src\assets\linked.png"></img></a>
+          </div><br /><br />
+          <div className="flex justify-between">
+            <a href="#"><span className=" text-xs text-gray-600 hover:text-gray-950">Terms Of Use</span></a>
+            <a href="#"><span className=" text-xs text-gray-600 hover:text-gray-950">Privacy Policy</span></a>
+            <a href="#"><span className=" text-xs text-gray-600 hover:text-gray-950">Copyrights</span></a>
+            <a href="#"><span className=" text-xs text-gray-600 hover:text-gray-950">About Athena</span></a>
+            <a href="#"><span className=" text-xs text-gray-600 hover:text-gray-950">Get Help</span></a>
+            <a href="#"><span className=" text-xs text-gray-600 hover:text-gray-950">Address</span></a>
+          </div>
         </div>
         <div className="img-container p-2 ">
           <img className=" w-48 h-full float-right " src="src\assets\ISTE original logo black 1(2).png" alt="" />
