477640ee893a13dfc263d79e3c9f06284d203172
thejzain <thejzainkp@gmail.com>
2023-12-30 21:08:33 +0530

    events page

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,12 @@
-export default function Card({slides}) {
+import eventsDetails from "../content/events.js"
+export default function Card({eventid}) {
+  const event = eventsDetails.find((e)=> e.eventid == eventid)
   return(
-    <div className="embla__slide p-5" >
+    <div className="embla__slide p-5 w-[400px]" >
       <div
         className="embla__slide__img h-96 bg-white rounded-lg text-black"
       >
-        hello
+        hello {event.name}
       </div>
     </div>
   )
--- src/routes/events.jsx
+++ src/routes/events.jsx
@@ -1,6 +1,25 @@
+import eventsDetais from "../content/events.js"
+import Card from "../components/card.jsx"
+import CustomH from "../components/customh.jsx"
 export default function Events() {
+  const eventdata_d1 = eventsDetais.filter((e)=> {
+    return e.date == "19-01-2024"
+  })
   return(
-    <div>Events</div>
+    <div><CustomH name="Events" type="1"/>
+      <h2>Day 1</h2>
+      <div>{eventdata_d1.map((event)=>{
+        return(
+        <Card eventid={event.eventid}/> 
+        )
+      })}</div>
+      <h2>Day 2</h2>
+      <div>{eventdata_d1.map((event)=>{
+        return(
+        <Card eventid={event.eventid}/> 
+        )
+      })}</div>
+    </div>
   )
   
 }
