4799f4e11944695c646851616fda0fb084abc214
thejzain <thejzainkp@gmail.com>
2023-12-30 17:57:36 +0530

    navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route, useParams} from "react-router-dom"
 import Home from "./routes/home.jsx"
 import Footer from "./components/footer.jsx"
 import EventPage from "./routes/event.jsx"
+import NavBar from "./components/navbar.jsx"
 
 export default function App() {
   return(
     <div className="bg-black text-white">
       <BrowserRouter>
+        <NavBar/>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="event/:eventid" element={<EventPage/>}/>
