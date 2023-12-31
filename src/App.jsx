f9196f3f7b094dc91fe565dca1ee549ac91cdf19
thejzain <thejzainkp@gmail.com>
2023-12-31 17:31:23 +0530

    0

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Schedule from "./routes/schedule.jsx"
 
 export default function App() {
   return(
-    <div className="bg-black text-white">
+    <div className="bg-black text-white min-h-screen">
       <BrowserRouter>
         <NavBar/>
         <Routes >
