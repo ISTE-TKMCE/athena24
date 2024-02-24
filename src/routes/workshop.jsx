d2ce89a3476369bf8a03facbd89785532dfce670
thejzain <thejzainkp@gmail.com>
2024-02-24 20:00:46 +0530

    details page changes

--- src/routes/workshop.jsx
+++ src/routes/workshop.jsx
@@ -17,21 +17,21 @@ export default function WorkshopPage() {
         <div className="mix-blend-lighten font-primary text-center text-3xl md:text-4xl m-5">
           {event.name}
         </div>
-      <div className="grid grid-rows-2 gap-y-2 h-1/2 md:grid md:grid-cols-2 md:h-96 font-mono text-lg">
+      <div className="text-left grid grid-rows-2 gap-y-2 h-1/2 md:grid md:grid-cols-2 md:h-96 font-mono text-lg">
         <div className="flex justify-center ">
           <img className="w-64" src={event.posters[0]} />
         </div>
         <div className="flex justify-center">
           <div className="">
             <div className="mb-2">
-              <span className="text-3xl font-semibold font-serif ml-16">
+              <span className="text-3xl font-semibold font-serif underline">
                 Description
               </span>
               <div className="mt-4">{event.details}</div>
             </div>
             <div className="mt-14">
-              <div>Date : {event.date}</div>
-              <div>Coordinators :</div>
+              <div className="font-bold">Date : {event.date}</div>
+              <div className="underline">Coordinators </div>
               <div>
                 {event.coordinators[0].name}: {event.coordinators[0].phone}
               </div>
