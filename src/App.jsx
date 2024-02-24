1f5a7a15f8c569ae976a2b3d1b2cc126e0ee3787
thejzain <thejzainkp@gmail.com>
2024-02-24 17:29:57 +0530

    workshop added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Events from "./routes/events.jsx"
 import Workshops from "./routes/workshops.jsx";
 import Schedule from "./routes/schedule.jsx"
 import Details from "./routes/details.jsx"
+import WorkshopPage from "./routes/workshop.jsx"
 
 export default function App() {
   return(
@@ -17,6 +18,7 @@ export default function App() {
         <Routes >
           <Route path="/" element={<Home/>} />
           <Route path="/event/:eventid" element={<EventPage/>}/>
+          <Route path="/workshop/:eventid" element={<WorkshopPage/>}/>
           <Route path="/events" element={<Events/>}/>
           <Route path="/workshops" element={<Workshops/>}/>
           <Route path="/schedule" element={<Schedule/>}/>
---
+++ src/content/workshops.js
@@ -0,0 +1,45 @@
+const workshopDetails= [{
+  id: "3d-printing",
+  name : "testname",
+  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+  date : "19-01-2024",
+  posters: ["/vite.svg"],
+  price : 200,
+  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}],
+  link: "https://bit.ly/ATHENA_3D_PRINTING"
+},{
+  id: "iot-automation",
+  name : "testname",
+  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+  date : "19-01-2024",
+  posters: [""],
+  price : 200,
+  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}],
+    link: "https://bit.ly/_iot_automation"
+},{
+  id: "id3",
+  name : "testname",
+  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+  date : "19-01-2024",
+  posters: [""],
+  price : 200,
+  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
+},{
+  id: "id3",
+  name : "testname",
+  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+  date : "19-01-2024",
+  posters: [""],
+  price : 200,
+  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
+},{
+  id: "id3",
+  name : "testname",
+  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+  date : "19-01-2024",
+  posters: [""],
+  price : 200,
+  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
+},
+]
+export default workshopDetails;
