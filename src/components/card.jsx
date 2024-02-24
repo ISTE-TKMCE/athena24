d3289f8f6e75404c5f9abb6291d5177270143a66
thejzain <thejzainkp@gmail.com>
2024-02-24 18:15:39 +0530

    fix

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -33,7 +33,7 @@ return(
         className="group embla__slide__img h-96 bg-white rounded-lg text-black grid place-content-center"
       >
         <img src={event.posters[0]} className="object-fill w-full h-full"/>
-        <Link to={'/event/'+ event.id} className="absolute bottom-10 left-28 border border-yellow-300 w-36 rounded-lg text-center drop-shadow-lg bg-yellow-300 font-primary text-xl px-2 py-1">Register</Link>
+        <Link to={'/workshop/'+ event.id} className="absolute bottom-10 left-28 border border-yellow-300 w-36 rounded-lg text-center drop-shadow-lg bg-yellow-300 font-primary text-xl px-2 py-1">Register</Link>
       </div>
     </div>
   )
