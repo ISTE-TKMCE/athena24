87c02ed966ef1cd91c37f1f88e1d7502bc15e18a
DEVANAND.S <133562029+Devanand999@users.noreply.github.com>
2024-02-24 22:58:00 +0530

    sd

--- src/content/events.js
+++ src/content/events.js
@@ -1,9 +1,9 @@
 const eventsDetails = [{
   id: "id",
   name : "testname",
-  details:"jhfoufh jhudhfuyfhl",
+  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
   date : "19-01-2024",
-  posters: ["/vite.svg"],
+  posters: ["/Elon Test.jpg"],
   price : 200,
   coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
 },{
