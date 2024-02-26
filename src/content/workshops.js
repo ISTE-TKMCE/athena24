6cb9d1023013487ad8992edc517a17d039bcbde1
thejzain <thejzainkp@gmail.com>
2024-02-26 20:42:02 +0530

    more fix include responsiveness

--- src/content/workshops.js
+++ src/content/workshops.js
@@ -3,23 +3,33 @@ const workshopDetails= [{
   name : "Modelling and 3D Printing Workshop",
   details: "Enter into the world where ideas turn into reality with Modelling and 3D Printing Workshop, as a part of ATHENA under the mentorship of the renowned maestro, Anas T.Understand prototype designing, product customization and many more concepts using CAD. Unleash your talents and design the products of the future with us by joining this workshop. Let's dive deep in to create a future with the most efficient products designed by ourselves to satiate our wants and wishes.",
   date : "17 March 2024",
-  posters: ["/3dm.jpg","/syllabusss_02.jpg","/2_02.jpg"],
+  posters: ["/new3d.jpg","/new3d1.jpg","/new3d3.jpg"],
+  deadline : "8 March 2024",
   price : 300,
   coordinators : [{name: "Lakshmi", phone:"8301064679"},{name: "Anand", phone:"8593085041"}],
-  link: "https://bit.ly/ATHENA_3D_PRINTING",
-  deadline : "8 March 2024"
+  link: "https://bit.ly/ATHENA_3D_PRINTING"
 },{
   id: "iot-automation",
   name : "IoT Automation",
   details: "Embark on a technological journey with IoT Automation Workshop as part of ATHENA, led by industry expert Rajasekharan A H. Learn to master voice-controlled lights, sensor-adjusted comfort, and anticipatory devices using NodeMCU, Raspberry Pi, Blynk & Thingspeak.\nUnlock your potential and create smarter solutions by joining us in this workshop. Explore the possibilities of a world where devices seamlessly respond to your needs.",
   date : "16 March 2024",
-  posters: ["/3d.jpg","/syllabusss_03.jpg","/2_03.jpg"],
+  deadline : "8 March 2024",
+  posters:  ["/3d.jpg","/syllabusss_03.jpg","/2_03.jpg"],
   price : 400,
   coordinators : [{name: "S Suraj", phone:"7736287859"},{name: "Anjana", phone:"9633944709"}],
-    link: "https://bit.ly/_iot_automation",
-  deadline : "8 March 2024"
+  link: "https://bit.ly/_iot_automation"
 },{
   id: "id3",
+  name : "Driving System",
+  details: "Explore the evolution of autonomous driving technology at the Autonomous Driving System Worksop at Athena, the 22nd Annual State Student Convention of ISTE Kerala Section. Under the guidance of Mr. Midhun M, founder of Revertech IT Solutions, engage in hands-on experiences with sensor technology and master machine learning concepts.  ",
+  date : "16 March 2024 & 17 March 2024",
+  deadline : "8 March 2024",
+  posters: ["/ads3_01.jpg","/ads2_01.jpg","/ads1_01.jpg"],
+  price : 200,
+  coordinators : [{name: "Risal", phone:"9188537198"},{name: "Sinan", phone:"9526075042"}],
+  link: "https://bit.ly/ATHENA_DRIVING_SYSTEM"
+},{
+  id: "id4",
   name : "testname",
   details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
   date : "19-01-2024",
@@ -27,15 +37,7 @@ const workshopDetails= [{
   price : 200,
   coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
 },{
-  id: "id3",
-  name : "testname",
-  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-  date : "19-01-2024",
-  posters: [""],
-  price : 200,
-  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
-},{
-  id: "id3",
+  id: "id5",
   name : "testname",
   details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
   date : "19-01-2024",
