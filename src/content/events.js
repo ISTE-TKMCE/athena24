7bf641a3076638bdc3011dc65b5eb283b29c667a
thejzain <thejzainkp@gmail.com>
2024-02-27 20:11:22 +0530

    eureeka added

--- src/content/events.js
+++ src/content/events.js
@@ -1,11 +1,13 @@
 const eventsDetails = [{
-  id: "id",
-  name : "testname",
-  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-  date : "19-01-2024",
-  posters: ["/vite.svg"],
-  price : 200,
-  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
+  id: "eureka",
+  name : "EUREKA",
+  details: "You've heard the legend of Archimedes' triumphant 'Eureka!', but what if you could be the next to experience that thrill of discovery? At ATHENA, the 22nd Annual State Students' Convention of ISTE Kerala Section, hosted by ISTE TKMCE, that chance awaits you! Join us for EUREKA!, an electrifying Ideathon event where your innovative spirit can take flight. Pitch game-changing solutions, wow the judges and collaborate in an auction round for a unique twist. Archimedes had his bath, you have your chance! Dive into innovation at EUREKA! and make a splash at ATHENA.",
+  date : "16 March 2024",
+  posters: ["/EUREKA flagship final.jpg","/EUREKA flagship final guidelines.jpg"],
+  price : 100,
+  coordinators : [{name: "Thameem", phone:"7736183501"},{name: "Nandana", phone:"7012636518"}],
+  link:"https://bit.ly/-Eureka-",
+  deadline : "nil"
 },{
   id: "id2",
   name : "testname",
