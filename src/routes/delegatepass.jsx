ccdbe3530379fa66f46c7dfc3d5738ae1ae0d8c2
Kiran-347 <kiran333krishna@gmail.com>
2024-02-28 19:36:54 +0530

    delegate pass

--- src/routes/delegatepass.jsx
+++ src/routes/delegatepass.jsx
@@ -20,7 +20,7 @@ const blur2Coordinates = isMobile
       <div className=" relative grid justify-items-center gap-5">
         <img src={poster} className="px-5 md:w-[500px]"/>
         <div className="text-xl text-center px-10 md:px-20" >Delegate passes are available for 400/-. The pass can be used for up to five events per delegate as well as talk sessions. Workshops are not included in this.</div>
-      <Link to={"https://www.yepdesk.com/embed/buy-tickets/65d5ba7cc9e77c000171d68d/private/ch3mnatd0l"}><div className="my-5 mb-10 bg-yellow-300 rounded-xl text-black font-primary w-52 px-5 py-2 text-xl text-center">Get Pass</div></Link>
+      <Link to={"https://www.yepdesk.com/athena-22nd-annual-state-students-convention-of-iste-kerala-section"}><div className="my-5 mb-10 bg-yellow-300 rounded-xl text-black font-primary w-52 px-5 py-2 text-xl text-center">Get Pass</div></Link>
       </div>
     </div>
   )
