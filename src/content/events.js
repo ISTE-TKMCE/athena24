47900d768fc0a3f6e5b47eccf5711db80d00b2e5
Kiran-347 <kiran333krishna@gmail.com>
2024-02-28 22:08:18 +0530

    update

--- src/content/events.js
+++ src/content/events.js
@@ -14,7 +14,7 @@ const eventsDetails = [{
   details: "Dive into a series of practical challenges with Relay Coding as a part of ATHENA, 22nd Annual State Student Convention of Kerala Section putting your coding agility to test with real-world scenarios. Apply your coding know-how to solve challenges inspired by everyday situations, blending theory with real-world applications.Join forces with fellow coders for a collaborative coding experience and compete for coveted prizes and recognition.Engage in real-life problem-solving, amplify your coding acumen, and be part of a dynamic coding community.",
   date : "16 March 2024",
   posters: ["/relay (1).jpg","/relay giudelines.png"],
-  price : "",
+  price : "need delegate pass",
   coordinators : [{name: "Devakrishna A S", phone:"7736784259"},{name: "Joel Paul ", phone:"9447359118"}],
   link:"https://www.yepdesk.com/relay-coding1",
   deadline: "11 March 2024"
@@ -23,7 +23,7 @@ const eventsDetails = [{
   name : "Cipher Cell",
   details: "Decipher, Unwind, Triumph: Embark on a Coding Odyssey!Ready to unlock your coding prowess and conquer a thrilling challenge? Brace yourselves for Cipher Cell – an event that will test your problem-solving skills, coding acumen and teamwork like never before! Prepare to decipher cryptic clues, unwind code knots and triumph over mind-bending tasks.Don't miss this exhilarating opportunity to sharpen your coding skills and to engage with a vibrant community of coders and enthusiasts.Unleash your inner coding warrior and claim the ultimate triumph!",
   date : "17 March 2024",
-  posters: ["/cypher cell.jpg","/cypher cell guides.png"],
+  posters: [""],
   price : "",
   coordinators : [{name: "Anandhu", phone:" 9074669356"},{name: "Eldho", phone:" 9188131235"}],
   link:"https://www.yepdesk.com/cipher-cell",
