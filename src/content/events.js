e54bb34959b9e864080b7fc618b2c22c0bd56290
Kiran-347 <kiran333krishna@gmail.com>
2024-02-28 22:19:29 +0530

    price update

--- src/content/events.js
+++ src/content/events.js
@@ -4,7 +4,7 @@ const eventsDetails = [{
   details: "You've heard the legend of Archimedes' triumphant 'Eureka!', but what if you could be the next to experience that thrill of discovery? At ATHENA, the 22nd Annual State Students' Convention of ISTE Kerala Section, hosted by ISTE TKMCE, that chance awaits you! Join us for EUREKA!, an electrifying Ideathon event where your innovative spirit can take flight. Pitch game-changing solutions, wow the judges and collaborate in an auction round for a unique twist. Archimedes had his bath, you have your chance! Dive into innovation at EUREKA! and make a splash at ATHENA.",
   date : "16 March 2024",
   posters: ["/EUREKA flagship final.jpg","/EUREKA_flagship_final final.png"],
-  price : 100,
+  price : "100 RS",
   coordinators : [{name: "Thameem", phone:"7736183501"},{name: "Nandana", phone:"7012636518"}],
   link:"https://bit.ly/-Eureka-",
   deadline : "4 March 2024"
@@ -14,7 +14,7 @@ const eventsDetails = [{
   details: "Dive into a series of practical challenges with Relay Coding as a part of ATHENA, 22nd Annual State Student Convention of Kerala Section putting your coding agility to test with real-world scenarios. Apply your coding know-how to solve challenges inspired by everyday situations, blending theory with real-world applications.Join forces with fellow coders for a collaborative coding experience and compete for coveted prizes and recognition.Engage in real-life problem-solving, amplify your coding acumen, and be part of a dynamic coding community.",
   date : "16 March 2024",
   posters: ["/relay (1).jpg","/relay giudelines.png"],
-  price : "need delegate pass",
+  price : "This Event Comes Under Delegate Pass",
   coordinators : [{name: "Devakrishna A S", phone:"7736784259"},{name: "Joel Paul ", phone:"9447359118"}],
   link:"https://www.yepdesk.com/relay-coding1",
   deadline: "11 March 2024"
@@ -24,7 +24,7 @@ const eventsDetails = [{
   details: "Decipher, Unwind, Triumph: Embark on a Coding Odyssey!Ready to unlock your coding prowess and conquer a thrilling challenge? Brace yourselves for Cipher Cell – an event that will test your problem-solving skills, coding acumen and teamwork like never before! Prepare to decipher cryptic clues, unwind code knots and triumph over mind-bending tasks.Don't miss this exhilarating opportunity to sharpen your coding skills and to engage with a vibrant community of coders and enthusiasts.Unleash your inner coding warrior and claim the ultimate triumph!",
   date : "17 March 2024",
   posters: [""],
-  price : "",
+  price : "This Event Comes Under Delegate Pass",
   coordinators : [{name: "Anandhu", phone:" 9074669356"},{name: "Eldho", phone:" 9188131235"}],
   link:"https://www.yepdesk.com/cipher-cell",
   deadline:"nil"
--- src/routes/event.jsx
+++ src/routes/event.jsx
@@ -29,7 +29,7 @@ export default function EventPage() {
               </span>
               <div className="mt-4">{event.details}</div>
             </div>
-            <div className="text-xl font-bold text-yellow-300 mt-4">Reg.Fee : {event.price}Rs</div>
+            <div className="text-xl font-bold text-yellow-300 mt-4">Reg.Fee : {event.price}</div>
             <div className="text-xl font-bold text-yellow-300 mt-2">Deadline: {event.deadline}</div>
             <div className="mt-10">
               <div className="font-bold my-2 text-lg">Date : {event.date}</div>
