2628cab51ddc7f949d8aebc22de52ffbf49ac56b
Akash Prasad <akashprasadmk@gmail.com>
2024-03-05 17:20:18 +0530

    updates

--- src/content/events.js
+++ src/content/events.js
@@ -164,7 +164,7 @@ const eventsDetails = [{
   details: "",
   date : "nil",
   posters: [],
-  price : "This Event Comes Under Delegate Pass",
+  price : "",
   coordinators : [{name: "nil", phone:"nil"},{name: "nil", phone:"nil"}],
   // link: "https://www.yepdesk.com/volt-hunt/private/7j99pjtc0",
   deadline:"nil"
@@ -174,7 +174,7 @@ const eventsDetails = [{
   details: "",
   date : "nil",
   posters: [],
-  price : "This Event Comes Under Delegate Pass",
+  price : "",
   coordinators : [{name: "nil", phone:"nil"},{name: "nil", phone:"nil"}],
   // link: "https://www.yepdesk.com/rendercraft/private/3b0epgupn5",
   deadline:"nil"
