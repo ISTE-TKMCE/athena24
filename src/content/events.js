0eaabb5c00a813cd84db4448bf3502a9bcda6d46
Akash Prasad <akashprasadmk@gmail.com>
2024-03-06 22:19:08 +0530

    update

--- src/content/events.js
+++ src/content/events.js
@@ -183,7 +183,7 @@ const eventsDetails = [{
   name : "PavilX",
   details: "PavilX, presented by ATHENA, 22nd Annual State Students' Convention of ISTE Kerala Section, invites participants to redefine art of architecture through creativity and innovation. The competition shows uniqueness, challenging participants to craft pavilions that showcases their individual perspectives and integrate the theme unveiled at the event. Get set to showcase your architectural and design abilities.",
   date : "17 March 2024",
-  posters: [""],
+  posters: [],
   price : "This Event Comes Under Delegate Pass",
   coordinators : [{name: "Meenakshi K Prasannan", phone:"7306690336"},{name: "Muhammed Bin Shafeeq T P", phone:"9061310160"}],
   link: "https://www.yepdesk.com/pavilx/private/s9hotoan7e",
