e32f01db361d7b79272758f7a832e0cb71fa5b49
Akash Prasad <akashprasadmk@gmail.com>
2024-03-10 11:36:19 +0530

    talkshow added

--- src/content/talkshow.js
+++ src/content/talkshow.js
@@ -1,10 +1,23 @@
 const talkshowDetails = [{
-  id: "id",
-  name : "testname",
+  id: "gtechtalk",
+  name : "GEOSPATIAL TECHNOLOGY TALK SESSION",
   details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
   date : "19-01-2024",
-  posters: ["/vite.svg"],
-  price : 200,
-  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}]
-},]
+  posters: [],
+  price : "Included with Delegate Pass",
+  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}],
+  link: "https://www.yepdesk.com/geospatial-technology-concept-&-applications/private/83k76sqa9k"
+},
+{
+  id: "manuwritetalk",
+  name : "MANUSCRIPT WRITING TALK SESSION",
+  details: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+  date : "19-01-2024",
+  posters: [],
+  price : "Included with Delegate Pass",
+  coordinators : [{name: "hehe", phone:"99999999"},{name: "hehe", phone:"99999999"}],
+  link: "https://www.yepdesk.com/an-introduction-to-research-and-tips-for-manuscript-writing/private/bg5a1oh2mj"
+},
+
+]
 export default talkshowDetails;
