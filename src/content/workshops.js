741758c34d0600150a4fa441fd3698bf5ff79679
DEVANAND.S <133562029+Devanand999@users.noreply.github.com>
2024-03-13 08:30:41 +0530

    date extended

--- src/content/workshops.js
+++ src/content/workshops.js
@@ -4,7 +4,7 @@ const workshopDetails= [{
   details: "Enter into the world where ideas turn into reality with Modelling and 3D Printing Workshop, as a part of ATHENA under the mentorship of the renowned maestro, Anas T.Understand prototype designing, product customization and many more concepts using CAD. Unleash your talents and design the products of the future with us by joining this workshop. Let's dive deep in to create a future with the most efficient products designed by ourselves to satiate our wants and wishes.",
   date : "17 March 2024",
   posters: ["/new3d.jpg","/new3d1.jpg","/new3d3.jpg"],
-  deadline : "13 March 2024",
+  deadline : "14 March 2024",
   price : 300,
   coordinators : [{name: "Lakshmi", phone:"8301064679"},{name: "Anand", phone:"8593085041"}],
   link: "https://bit.ly/ATHENA_3D_PRINTING"
@@ -24,7 +24,7 @@ const workshopDetails= [{
   details: "In this era of cutting-edge artificial intelligence, if you've ever been captivated by the concept of autonomous driving cars, then this workshop is designed just for you!💫Dive into the evolution and current landscape of autonomous driving technology at the Autonomous Driving System Workshop at Athena, the 22nd Annual State Students' Convention of ISTE Kerala Section.🤩Immerse yourself in a range of activities, from hands-on experiences with sensor technology to mastering Machine Learning concepts.✨",
   date : "16 and 17 of March 2024",
   posters: ["/ads3_01.jpg","/ads2_01.jpg","/ads1_01.jpg"],
-  deadline : "13 March 2024",
+  deadline : "14 March 2024",
   price : 600,
   coordinators : [{name: "Risal", phone:"9188537198"},{name: "Sinan", phone:"9526075042"}],
   link:"https://docs.google.com/forms/d/e/1FAIpQLSe7AGL6sde6VLxeBwwGV8qrq8XRBV2f9HHu5LxqtMimCFlxSg/viewform"
